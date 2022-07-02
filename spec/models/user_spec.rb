141cc6a351228383e342bf7ec1437fffbc180c30
Thermiee <navyblack16@yahoo.com>
2022-07-02 15:49:09 +0100

    Set up test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,36 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before :example do
-    @user = User.create(name: 'Temmy')
-    @operation = Operation.create(name: 'Test', author: @user)
+    @user = FactoryBot.create(:user, :confirmed)
   end
 
-  it 'should have a name' do
-    expect(@user.name).to eq('Temmy')
+  it 'should be valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  it 'should be invalid without a name' do
+    @user.name = nil
+
+    expect(@user).to be_invalid
+
+    @user.name = ''
+
+    expect(@user).to be_invalid
+  end
+
+  it 'should be invalid with a name over 100 characters' do
+    @user.name = 'a' * 101
+
+    expect(@user).to be_invalid
+  end
+
+  it 'should be invalid without an email' do
+    @user.email = nil
+
+    expect(@user).to be_invalid
+
+    @user.email = ''
+
+    expect(@user).to be_invalid
   end
 end
