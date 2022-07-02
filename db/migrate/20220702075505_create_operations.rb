6244bcc7bbf5a7bd5ff5e4570768c8938025d500
Thermiee <navyblack16@yahoo.com>
2022-07-02 11:34:02 +0100

    update schema

--- db/migrate/20220702075505_create_operations.rb
+++ db/migrate/20220702075505_create_operations.rb
@@ -1,6 +1,9 @@
 class CreateOperations < ActiveRecord::Migration[7.0]
   def change
     create_table :operations do |t|
+      t.string :name, null: false
+      t.integer :amount, null: false
+      t.references :group, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_02_082357) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_02_092259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "budgets", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
-  create_table "groups", force: :cascade do |t|
+  create_table "groups_operations", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "operation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_groups_operations_on_group_id"
+    t.index ["operation_id"], name: "index_groups_operations_on_operation_id"
   end
 
   create_table "operations", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "amount", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_operations_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "operations", "users", column: "author_id"
 end
