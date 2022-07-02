a72cfb554d4a854a408153548b2de57015597a34
Thermiee <navyblack16@yahoo.com>
2022-07-02 14:06:10 +0100

    Design layout for homepage

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :manage, Operation, author_id: user.id
     can :manage, Group, author_id: user.id
+    can :manage, Operation, author_id: user.id
   end
 end
